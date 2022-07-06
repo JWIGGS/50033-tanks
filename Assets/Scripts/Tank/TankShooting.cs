4f08f63aa2ffaa880197889ee84a96e381bf21c3
JWIGGS <5955038+JWIGGS@users.noreply.github.com>
2022-07-06 21:40:10 +0800

    Improvement 1

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -6,78 +6,60 @@ public class TankShooting : MonoBehaviour
     public int m_PlayerNumber = 1;
     public Rigidbody m_Shell;
     public Transform m_FireTransform;
-    public Slider m_AimSlider;
     public AudioSource m_ShootingAudio;
     public AudioClip m_ChargingClip;
     public AudioClip m_FireClip;
-    public float m_MinLaunchForce = 15f;
-    public float m_MaxLaunchForce = 30f;
-    public float m_MaxChargeTime = 0.75f;
+
+    public GameObject m_TurretObject;
+
+    public float m_FireRate;
+    public float m_ShellSpeed;
 
     private string m_FireButton;
-    private float m_CurrentLaunchForce;
-    private float m_ChargeSpeed;
-    private bool m_Fired;
     private float nextFireTime;
 
-    private void OnEnable()
-    {
-        m_CurrentLaunchForce = m_MinLaunchForce;
-        m_AimSlider.value = m_MinLaunchForce;
-    }
 
 
     private void Start()
     {
         m_FireButton = "Fire" + m_PlayerNumber;
-        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
     }
 
 
     private void Update()
     {
-        m_AimSlider.value = m_MinLaunchForce;
+        if (Input.GetButton(m_FireButton))
+        {
+            Fire(m_FireRate);
+        }
 
-        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        if (m_TurretObject != null)
         {
-            m_CurrentLaunchForce = m_MaxLaunchForce;
-            Fire(m_CurrentLaunchForce, 1);
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Plane p = new Plane(Vector3.up, m_TurretObject.transform.position);
+            if (p.Raycast(mouseRay, out float hitDist))
+            {
+                Vector3 hitPoint = mouseRay.GetPoint(hitDist);
+                m_TurretObject.transform.LookAt(hitPoint);
+            }
         }
-        else if (Input.GetButtonDown(m_FireButton))
-        {
-            m_Fired = false;
-            m_CurrentLaunchForce = m_MinLaunchForce;
 
-            m_ShootingAudio.clip = m_ChargingClip;
-            m_ShootingAudio.Play();
-        }
-        else if (Input.GetButton(m_FireButton) && !m_Fired)
-        {
-            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
-            m_AimSlider.value = m_CurrentLaunchForce;
-        }
-        else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
-        {
-            Fire(m_CurrentLaunchForce, 1);
-        }
+
     }
 
 
-    public void Fire(float launchForce, float fireRate)
+    public void Fire(float fireRate)
     {
         if (Time.time <= nextFireTime) return;
 
         nextFireTime = Time.time + fireRate;
-        m_Fired = true;
 
         Rigidbody shellInstance =
             Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
-        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+        shellInstance.velocity = m_ShellSpeed * m_FireTransform.forward;
 
         m_ShootingAudio.clip = m_FireClip;
         m_ShootingAudio.Play();
-
-        m_CurrentLaunchForce = m_MinLaunchForce;
     }
-    
+
 }
